ccddfdd8c5f33b8d8b6e21fb6ae46e995e3c7ebf
Antosz Jakub <jakub.antosz@microsoft.wsei.edu.pl>
2019-10-06 12:34:07 +0200

    Zadanie 2

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,32 @@
+function distFromAvarage(tab){
+
+    let sum = 0;
+    let newTab = [];
+
+    for(i=0; i<tab.lenght;i++){
+        sum = sum + tab[i];
+    }
+
+    sum = sum/tab.lenght;
+
+    tab.forEach(el =>{
+        newTab.push(el-sum);
+    })
+
+    return(newTab);
+
+}
+
+let tab = [1,2,3,4,5,6,7]
+
+distFromAvarage(tab);
+
+//Zadanie 1
+cost favouriteFruits = ['apple', 'orange', 'bannana'];
+
+console.log(favouriteFruits[0]);
+console.log(favouriteFruits[favouriteFruits.lenght; - 1]);
+    
+for (var i =0; i < favouriteFruits.lenght; i++){  
+    console.log(favouriteFruits[i]);
+}
